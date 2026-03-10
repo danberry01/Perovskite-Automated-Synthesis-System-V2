9690ecb55d52611427c27b30dfa0421a3a5cb37c
Daniel Countermine <dcounte1@binghamton.edu>
2026-03-10 12:18:12 -0400

    Functioning frame prioritization when changing window size

--- gui/frames/tab_view_frame.py
+++ gui/frames/tab_view_frame.py
@@ -7,40 +7,52 @@ class TabViewFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(
             master,
-            width = 10,
+            width = 70,
             height = 800,
             corner_radius = 0,
             fg_color = FOREGROUND_COLOR
         )
 
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
         
         self.file_button = ctk.CTkButton(
             master = self,
-            width = 30, 
+            width = 60, 
             height = 60, 
             corner_radius = 0,
             fg_color = PRIMARY_BUTTON_COLOR,
             hover_color = HOVER_BUTTON_COLOR,
-            text = "Top Button!!!"
+            text = "1",
+            font = ("Roboto", 10)
         )
         self.file_button.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "new")
 
         self.bottom_button = ctk.CTkButton(
             master = self,
-            width = 30, 
+            width = 60, 
             height = 60, 
             corner_radius = 0,
             fg_color = PRIMARY_BUTTON_COLOR,
             hover_color = HOVER_BUTTON_COLOR,
-            text = "Bottom Button!!!"
+            text = "2",
+            font = ("Roboto", 10)
         )
-        self.bottom_button.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "sew")
+        self.bottom_button.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "sew")
 
-        self.master.bind("<Configure>",self.hide_overlapping_frames(disp_frame = self.file_button, hide_frame = self.bottom_button))
+        self.master.bind("<Configure>",lambda event: self.hide_overlapping_frames(disp_frame = self.file_button, hide_frame = self.bottom_button))
 
-    def hide_overlapping_frames(self, disp_frame = None, hide_frame = None, event = None):
-        if is_overlapping(disp_frame, hide_frame):
-            hide_frame.grid_forget()
+    def hide_overlapping_frames(self, disp_frame = None, hide_frame = None, event=None):
+
+        self.update_idletasks()
+
+        required_height = (
+            disp_frame.winfo_reqheight() +
+            hide_frame.winfo_reqheight() +
+            20
+        )
+
+        if self.winfo_height() < required_height:
+            hide_frame.grid_remove()
         else:
-            hide_frame.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "new")
+            if not hide_frame.winfo_ismapped():
+                hide_frame.grid()
