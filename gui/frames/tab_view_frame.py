68aaccb21c0c5a0034d4aa8a5f09b413c1237ff5
Daniel Countermine <dcounte1@binghamton.edu>
2026-03-10 13:43:42 -0400

    Functioning Icon switching for button Hover. All methods to do so are currently in tab_view class

--- gui/frames/tab_view_frame.py
+++ gui/frames/tab_view_frame.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 from ..components.theme import *
 from ..components.helper_functions import is_overlapping
 
@@ -12,9 +13,12 @@ class TabViewFrame(ctk.CTkFrame):
             corner_radius = 0,
             fg_color = FOREGROUND_COLOR
         )
-
-        self.grid_rowconfigure(1, weight=1)
         
+        self.rowconfigure(1, weight = 1)
+        
+        self.unselected_file_image = self.path_to_ctk_image("gui/icons/unselected_file.png")
+        self.selected_file_image = self.path_to_ctk_image("gui/icons/selected_file.png")
+
         self.file_button = ctk.CTkButton(
             master = self,
             width = 60, 
@@ -32,12 +36,13 @@ class TabViewFrame(ctk.CTkFrame):
             width = 60, 
             height = 60, 
             corner_radius = 0,
-            fg_color = PRIMARY_BUTTON_COLOR,
-            hover_color = HOVER_BUTTON_COLOR,
-            text = "2",
-            font = ("Roboto", 10)
+            fg_color = FOREGROUND_COLOR,
+            hover_color = FOREGROUND_COLOR,
+            image = self.unselected_file_image,
+            text = ""
         )
         self.bottom_button.grid(row = 2, column = 0, padx = 5, pady = 5, sticky = "sew")
+        self.frame_image_bindings(self.bottom_button, self.unselected_file_image, self.selected_file_image)
 
         self.master.bind("<Configure>",lambda event: self.hide_overlapping_frames(disp_frame = self.file_button, hide_frame = self.bottom_button))
 
@@ -56,3 +61,17 @@ class TabViewFrame(ctk.CTkFrame):
         else:
             if not hide_frame.winfo_ismapped():
                 hide_frame.grid()
+    
+    def path_to_ctk_image(self, image_path):
+        if image_path is None:
+            raise ValueError("image_path must be provided")
+
+        return ctk.CTkImage(
+            light_image=Image.open(image_path),
+            dark_image=Image.open(image_path),
+            size=(40, 40)
+        )
+    
+    def frame_image_bindings(self, widget, image_normal, image_hover):
+        widget.bind("<Enter>", lambda e: widget.configure(image=image_hover))
+        widget.bind("<Leave>", lambda e: widget.configure(image=image_normal))
