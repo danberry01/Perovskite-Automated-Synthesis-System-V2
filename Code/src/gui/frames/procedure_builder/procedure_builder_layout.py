5cbaca9616a78efcdd4ecc676e7ca94dce0dba5a
Daniel Countermine <dcounte1@binghamton.edu>
2026-03-26 13:54:33 -0400

    fixed accidental removal of dispatcher from procedure_drafter_frame instance creation

--- Code/src/gui/frames/procedure_builder/procedure_builder_layout.py
+++ Code/src/gui/frames/procedure_builder/procedure_builder_layout.py
@@ -24,6 +24,7 @@ class ProcedureBuilderFrame(ctk.CTkFrame):
         # CustomTKinter Frames
         self.procedure_drafter_frame = ProcedureDrafterFrame(master = self, 
                                                              move_registry = self.move_registry,
+                                                             dispatcher = self.dispatcher,
                                                              procedure_handler = self.procedure_handler
                                                             )
         self.procedure_drafter_frame.grid(row = 0, column = 0, rowspan = 3, padx = 10, pady = 10, sticky = "nsew")
