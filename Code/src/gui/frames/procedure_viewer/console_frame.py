f44f1a4a834e6d02e2d8356c903d450216709b62
Daniel Countermine <dcounte1@binghamton.edu>
2026-03-27 11:50:13 -0400

    fixed duplicate parameter

--- Code/src/gui/frames/procedure_viewer/console_frame.py
+++ Code/src/gui/frames/procedure_viewer/console_frame.py
@@ -52,7 +52,7 @@ class ConsoleFrame(ctk.CTkFrame):
             corner_radius=0,
             fg_color = PLAIN_TEXT_COLOR,
             state="disabled")
-        self.console.grid(row=1, column=0, columnspan = 2, padx=20, pady=10, columnspan=2, sticky="nsew")
+        self.console.grid(row=1, column=0, columnspan = 2, padx=20, pady=10, sticky="nsew")
         
         self.console.tag_config("DEBUG", foreground="#8df564")
         self.console.tag_config("WARNING", foreground="#e4f089")
