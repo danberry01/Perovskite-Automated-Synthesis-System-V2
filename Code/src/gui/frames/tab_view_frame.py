98b0b0a699f8d0955c5d3477b680537851425968
Daniel Countermine <dcounte1@binghamton.edu>
2026-03-30 13:56:52 -0400

    Added queue to the wrong frame

--- Code/src/gui/frames/tab_view_frame.py
+++ Code/src/gui/frames/tab_view_frame.py
@@ -28,15 +28,15 @@ class TabViewFrame(ctk.CTkFrame):
             master = self, 
             dispatcher = self.dispatcher,
             move_registry = self.move_registry, 
-            procedure_handler = self.procedure_handler
+            procedure_handler = self.procedure_handler,
+            queue_frame = None
         )
         self.procedure_builder_frame.grid(row = 0, column = 0, padx = 0, pady = 0, sticky = "nsew")
         self.procedure_builder_frame.grid_remove()
 
         self.procedure_viewer_frame = ProcedureViewerFrame(
             master = self,
-            procedure_handler = self.procedure_handler,
-            queue_frame = None
+            procedure_handler = self.procedure_handler
             )
         
         self.procedure_viewer_frame.grid(row = 0, column = 0, padx = 0, pady = 0, sticky = "nsew")
